ee43197e2c6aff509843c281e3ea8552f42c5648
Farid Mammadbayov <farid.mammadbayov@yrbrands.com>
2023-08-25 18:34:16 +0200

    unwanted bold fonts fix

--- scripts/content.js
+++ scripts/content.js
@@ -1,21 +1,30 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log("received");
   console.log(document.getElementById("ContentFrame"));
-  var iframe = document.getElementById("ContentFrame");
-  var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-  var target = innerDoc.getElementById("accelerator-page");
-  console.log(
-    target.children[0].children[0].innerText.split("... ").pop().split("Av")[0]
-  );
 
-  var url = target.children[0].children[0].innerText
-    .split("... ")
-    .pop()
-    .split("Av")[0];
+  if (document.getElementById("ContentFrame")) {
+    var iframe = document.getElementById("ContentFrame");
+    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+    var target = innerDoc.getElementById("accelerator-page");
+    console.log(target.querySelector(".info-banner"));
+    console.log(
+      target
+        .querySelector(".info-banner")
+        .children[0].innerText.split("... ")
+        .pop()
+        .split("Av")[0]
+    );
 
-  url = url.split("/home")[1];
-  console.log(url);
-  sendResponse(url);
+    var url = target
+      .querySelector(".info-banner")
+      .children[0].innerText.split("... ")
+      .pop()
+      .split("Av")[0];
+
+    url = url.split("/home")[1];
+    console.log(url);
+    sendResponse(url);
+  }
 
   return true;
 });
